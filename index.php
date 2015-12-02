bba89d212e5cbf3405309ac105e7b952d3def1a9
Conork23 <Conork23@gmail.com>
2015-12-02 17:32:40 +0000

    First Commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+	require('connect.php');
+    include('header.php');
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Swift KPI</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery -->
+    <script src="js/jquery-2.1.4.js"></script>
+    <!--Chart.js-->
+    <script src="js/Chart.js">
+	
+    </script>
+
+  </head>
+  <body>
+  		
+
+<div class="container">
+
+	 <div class="jumbotron">
+	 	<center><h1>This is the Home Page / DashBoard </h1></center>
+
+		
+
+	</div>
+
+</div> 
+
+
+
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
